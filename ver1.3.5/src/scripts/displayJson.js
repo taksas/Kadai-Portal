94c674e6584897d6d3b50f1f426cc0d7b869c33e
mimimimineko <80242131+mimimimineko@users.noreply.github.com>
2022-05-25 17:19:03 +0900

    Add files via upload

---
+++ ver1.3.5/src/scripts/displayJson.js
@@ -0,0 +1,12 @@
+﻿let manifestJsonData= require('../../manifest.json')
+
+function displayJsonContentId(){
+    displayJsonContent = document.querySelector("#versionNumber")
+    displayJsonContent.textContent = manifestJsonData.version
+}
+
+function displayJsonContentClass(displayJsonContent,displayJsonClass){
+    displayJsonClass="." + displayJsonId;
+    let displayJsonVersionInfo = document.querySelector(displayJsonClass);
+    displayJsonVersionInfo.textContent = displayJsonContent;
+}
