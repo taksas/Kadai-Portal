90abf0c6960516d83cc9ca43504424bd9a038f0d
mimimimineko <sleepsnoop1207@gmail.com>
2023-03-08 02:34:59 +0900

    first commit

--- ver1.2.0/src/scripts/moodleContentScripts.js
+++
@@ -1,6 +0,0 @@
-document.documentElement.style.setProperty('--color_primary',  localStorage.getItem('mainColorbyKadaiPortal'));
-chrome.runtime.sendMessage({greeting: "messageFromContentScripts!!"}, function(response) {
-  document.documentElement.style.setProperty('--color_primary',  response.color);
-  localStorage.setItem('mainColorbyKadaiPortal',response.color)
-  console.log(response.color);
-});
